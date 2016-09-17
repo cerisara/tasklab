c3ba985e6a5df7069a45dfa7a4c65866a3e2df53
xtof <cerisara@loria.fr>
2016-09-17 18:26:00 +0200

    read todo list OK

--- src/fr/xtof/tasklab/TaskLabAct.java
+++ src/fr/xtof/tasklab/TaskLabAct.java
@@ -1,13 +1,17 @@
 package fr.xtof.tasklab;
 
+import java.util.Arrays;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import java.lang.Exception;
 import cz.msebera.android.httpclient.*;
 import com.loopj.android.http.*;
 
 public class TaskLabAct extends Activity
 {
+    private final static String gitlabkey = "put your gitlab access token here";
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -19,7 +23,7 @@ public class TaskLabAct extends Activity
     /** Called when the user touches the button */
     public void geturl(View view) {
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://152.81.128.46:8888", new AsyncHttpResponseHandler() {
+        client.get("http://152.81.128.46:8888/api/v3/projects/88/repository/files?private_token="+gitlabkey+"&file_path=todo.txt&ref=master", new AsyncHttpResponseHandler() {
 
             @Override
             public void onStart() {
@@ -30,13 +34,36 @@ public class TaskLabAct extends Activity
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] response) {
                 // called when response HTTP status is "200 OK"
-                System.out.println("ON SUCCESS");
+                String str="OOO";
+                try {
+                    str = new String(response, "UTF-8"); // for UTF-8 encoding
+                    // encode
+                    //
+                    // decode
+                    // TODO parse the JSON
+                    int i=str.indexOf("content\":\"");
+                    if (i>=0) {
+                        i+=10;
+                        int j=str.indexOf("\"",i);
+                        byte[] tmp2 = Base64.decode(str.substring(i,j),Base64.DEFAULT);
+                        str = new String(tmp2, "UTF-8");
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println("ON SUCCESS "+str);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
                 // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                System.out.println("ON FAILURE");
+                String str="OOO";
+                try {
+                    str = new String(errorResponse, "UTF-8"); // for UTF-8 encoding
+                } catch (Exception ee) {
+                    ee.printStackTrace();
+                }
+                System.out.println("ON FAILURE "+str);
             }
 
             @Override
