6b1f68732e6053b465d0ac5cec30808e48a0daae
xtof <cerisara@loria.fr>
2016-09-18 18:35:52 +0200

    waiting msg OK

--- src/fr/xtof/tasklab/TaskLabAct.java
+++ src/fr/xtof/tasklab/TaskLabAct.java
@@ -3,7 +3,10 @@ package fr.xtof.tasklab;
 import java.util.Arrays;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.AsyncTask;
+import android.app.ProgressDialog;
 import android.view.View;
+import android.content.Context;
 import java.lang.Exception;
 import java.net.URLEncoder;
 import cz.msebera.android.httpclient.*;
@@ -11,107 +14,83 @@ import android.widget.ListView;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView;
+import android.widget.Toast;
 import com.loopj.android.http.*;
 
-public class TaskLabAct extends Activity
-{
+public class TaskLabAct extends Activity {
+    // TODO: check in the phone app storage if the config is here (secret keys + gtilab url); if not, let the user enter them in textfields
     private final static String gitlabkey = "";// put your gitlab access token here";
     ListView listView;
     String[] vals = {"<New Task>"};
+    static Context ctxt;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        ctxt=this;
         setContentView(R.layout.main);
         showList();
     }
     private void showList() {
-        // Get ListView object from xml
-        listView = (ListView) findViewById(R.id.list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, vals);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new OnItemClickListener() {
+        this.runOnUiThread(new Runnable() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int itemPosition     = position;
-                String  itemValue    = (String) listView.getItemAtPosition(position);
+            public void run() {
+                System.out.println("run SHOWLIST");
+                // Get ListView object from xml
+                listView = (ListView) findViewById(R.id.list);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(ctxt, android.R.layout.simple_list_item_1, vals);
+                listView.setAdapter(adapter);
+                System.out.println("run SHOWLIOOOOOOOOOOOOOOST");
+                listView.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        int itemPosition     = position;
+                        String  itemValue    = (String) listView.getItemAtPosition(position);
+                    }
+                });
             }
         });
     }
-
-    // TODO add feedback to the user while its trying to connect
 
     /** Called when the user touches the button */
     public void geturl(View view) {
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://152.81.128.46:8888/api/v3/projects/88/repository/files?private_token="+gitlabkey+"&file_path=todo.txt&ref=master", new AsyncHttpResponseHandler() {
-
-            @Override
-            public void onStart() {
-                // called before request is started
-                System.out.println("ON START");
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
-                // called when response HTTP status is "200 OK"
-                String str="OOO";
-                try {
-                    str = new String(response, "UTF-8"); // for UTF-8 encoding
-                    // encode
-                    //
-                    // decode
-                    // TODO parse the JSON
-                    int i=str.indexOf("content\":\"");
-                    if (i>=0) {
-                        i+=10;
-                        int j=str.indexOf("\"",i);
-                        byte[] tmp2 = Base64.decode(str.substring(i,j),Base64.DEFAULT);
-                        str = new String(tmp2, "UTF-8");
-                        if (str.charAt(str.length()-1)!='\n') str+='\n';
-                        str+="<New Task>";
-                        vals=str.split("\n");
-                        showList();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                System.out.println("ON SUCCESS "+str);
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                String str="OOO";
-                try {
-                    str = new String(errorResponse, "UTF-8"); // for UTF-8 encoding
-                } catch (Exception ee) {
-                    ee.printStackTrace();
-                }
-                System.out.println("ON FAILURE "+str);
-            }
-
-            @Override
-            public void onRetry(int retryNo) {
-                // called when request is retried
-                System.out.println("ON RETRY");
-            }
-        });
+        DetProgressTask dett = new DetProgressTask(0,"");
+        dett.execute();
     }
 
     public void putfile(View view) {
-        AsyncHttpClient client = new AsyncHttpClient();
         String s="";
         for (int i=0;i<vals.length-1;i++) { // not the last <new task>
             s+=vals[i]+'\n';
         }
+        s=s.substring(0,s.length()-1);
         try {
-            s=URLEncoder.encode(s.substring(0,s.length()-1),"UTF-8");
+            s=URLEncoder.encode(s,"UTF-8");
+            DetProgressTask dett = new DetProgressTask(1,s);
+            dett.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-            client.put("http://152.81.128.46:8888/api/v3/projects/88/repository/files?private_token="+gitlabkey+"&file_path=todo.txt&branch_name=master&content="+s+"&commit_message=update%20file", new AsyncHttpResponseHandler() {
 
+
+    private class DetProgressTask extends AsyncTask<String, Void, Boolean> {
+
+        private ProgressDialog dialog = new ProgressDialog(ctxt);
+        int getOuPut=0; // GET par defaut
+        String url;
+
+        public DetProgressTask(int getput, String url) {
+            this.getOuPut=getput;
+            this.url=url;
+        }
+
+        private void connect(final int typ, String url) {
+            SyncHttpClient client = new SyncHttpClient();
+            AsyncHttpResponseHandler rephdl = new AsyncHttpResponseHandler() {
                 @Override
                 public void onStart() {
                     // called before request is started
@@ -124,16 +103,25 @@ public class TaskLabAct extends Activity
                     String str="OOO";
                     try {
                         str = new String(response, "UTF-8"); // for UTF-8 encoding
+                        System.out.println("RESPONSEEEEEE "+str);
                         // encode
                         //
                         // decode
                         // TODO parse the JSON
+                        // TODO handle priorities
+                        // TODO manage conflicts
                         int i=str.indexOf("content\":\"");
                         if (i>=0) {
                             i+=10;
                             int j=str.indexOf("\"",i);
                             byte[] tmp2 = Base64.decode(str.substring(i,j),Base64.DEFAULT);
                             str = new String(tmp2, "UTF-8");
+                            if (typ==0) {
+                                if (str.charAt(str.length()-1)!='\n') str+='\n';
+                                str+="<New Task>";
+                                vals=str.split("\n");
+                                showList();
+                            }
                         }
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -158,9 +146,43 @@ public class TaskLabAct extends Activity
                     // called when request is retried
                     System.out.println("ON RETRY");
                 }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
+            };
+
+            if (typ==1) client.put("http://152.81.128.46:8888/api/v3/projects/88/repository/files?private_token="+gitlabkey+"&file_path=todo.txt&branch_name=master&content="+url+"&commit_message=update%20file", rephdl);
+            else client.get("http://152.81.128.46:8888/api/v3/projects/88/repository/files?private_token="+gitlabkey+"&file_path=todo.txt&ref=master", rephdl);
+        }
+
+        /** progress dialog to show user that the backup is processing. */
+
+        /** application context. */
+
+        protected void onPreExecute() {
+            this.dialog.setMessage("Please wait");
+            this.dialog.show();
+        }
+
+        protected Boolean doInBackground(final String... args) {
+            try {
+                connect(getOuPut,url);
+                return true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+
+            if (success) {
+                Toast.makeText(ctxt, "Successfully connected", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(ctxt, "Error", Toast.LENGTH_LONG).show();
+            }
         }
     }
 
