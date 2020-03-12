8e0e5658cdf4f88dc9bbc8c07defbe5f50ea86fb
CC <cerisara@loria.fr>
2020-03-12 12:46:25 +0100

    ok

--- oneapp4all/getText.py
+++ oneapp4all/getText.py
@@ -8,27 +8,55 @@ with urllib.request.urlopen(url) as f:
     html = f.read()
 """
 
-with open("arrivee-du-tableau-christ-enfants-vic-seille-1797712.html","r") as f: ss=f.readlines()
-html = '\n'.join(ss)
-i = html.find('text-chapo--text')
-html = html[i:]
-i = html.find('>')
-html = html[i+1:]
-soup = BeautifulSoup(html)
+def france3():
+    with open("arrivee-du-tableau-christ-enfants-vic-seille-1797712.html","r") as f: ss=f.readlines()
+    html = '\n'.join(ss)
+    i = html.find('text-chapo--text')
+    html = html[i:]
+    i = html.find('>')
+    html = html[i+1:]
+    soup = BeautifulSoup(html)
 
-# kill all script and style elements
-for script in soup(["script", "style"]):
-    script.extract()    # rip it out
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
 
-# get text
-text = soup.get_text()
+    # get text
+    text = soup.get_text()
 
-# break into lines and remove leading and trailing space on each
-lines = (line.strip() for line in text.splitlines())
-# break multi-headlines into a line each
-chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-# drop blank lines
-text = '\n'.join(chunk for chunk in chunks if chunk)
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
 
-print(text)
+    print(text)
+
+def zdnet():
+    with open("index.html","r") as f: ss=f.readlines()
+    html = '\n'.join(ss)
+    i = html.find("relatedContent")
+    html = html[i:]
+    i = html.find("div")
+    html = html[i+4:]
+    soup = BeautifulSoup(html)
+
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+
+    # get text
+    text = soup.get_text()
+
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+
+    print(text)
+
+zdnet()
 
--- oneapp4all/rss.py
+++ oneapp4all/rss.py
@@ -1,7 +1,7 @@
 import feedparser
 
 url = "https://www.zdnet.com/news/rss.xml"
-url = "https://france3-regions.francetvinfo.fr/grand-est/actu/rss"
+# url = "https://france3-regions.francetvinfo.fr/grand-est/actu/rss"
 feed = feedparser.parse(url)
 for it in feed['items']:
     print(it["title"])
