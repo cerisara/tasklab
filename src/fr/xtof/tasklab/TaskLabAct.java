c86ef652a6e43887d824a25d75c445666cb542fd
CC <cerisara@loria.fr>
2020-03-14 10:28:17 +0100

    back to previous scroll position

--- src/fr/xtof/tasklab/TaskLabAct.java
+++ src/fr/xtof/tasklab/TaskLabAct.java
@@ -62,6 +62,12 @@ public class TaskLabAct extends FragmentActivity {
     private RSS zdnetitems = new RSS();
     private RSS curitems = fr3items;
 
+    // action to perform when clicking on an item in the list
+    private int list2action = 0;
+
+    // position of the list in RSS items just before showing the details of one item
+    private int lastListPos = 0, top=0;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -77,6 +83,7 @@ public class TaskLabAct extends FragmentActivity {
             gitlabpwd=k;
         }
 
+        // show the last list displayed:
         getCurTasks();
 
         Intent in = this.getIntent();
@@ -144,6 +151,9 @@ public class TaskLabAct extends FragmentActivity {
     }
 
     private void showDetails(final int i) {
+        lastListPos = listView.getFirstVisiblePosition();
+        View v = listView.getChildAt(0);
+        top = (v == null) ? 0 : (v.getTop() - listView.getPaddingTop());
         vals.clear();
         vals.add(curitems.getTitle(i));
         vals.add(curitems.getSummary(i));
@@ -196,13 +206,52 @@ public class TaskLabAct extends FragmentActivity {
         dialog.show(getSupportFragmentManager(),"edit task");
     }
 
-
-    private interface TextConsumer {
-        public void newText(String s);
+    private void menuTODOlist() {
+        list2action = 2;
+        // TODO
     }
-
-    private void detdialog(String msg, TextConsumer fct) {
-
+    private void menuRSS(final String endpoint) {
+        list2action = 1;
+        new AlertDialog.Builder(this)
+            .setTitle("Download RSS "+endpoint)
+            .setMessage("Download RSS "+endpoint+" ?")
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    httpget("http://xolki.duckdns.org/"+endpoint+"?auth="+gitlabpwd);
+                }})
+        .setNegativeButton(android.R.string.no, null).show();
+    }
+    private void choixmenu(int i) {
+        switch(i) {
+            case 0:
+                menuRSS("rssf3");
+                break;
+            case 1:
+                menuRSS("rsszdnet");
+                break;
+            case 2:
+                menuRSS("rsshn");
+                break;
+            case 3:
+                menuTODOlist();
+                break;
+        }
+    }
+    public void menu(View v) {
+        list2action = 0;
+        setButtons("Menu","SETUP","FR3");
+        but2id=0;
+        vals.clear();
+        vals.add("RSS France 3");
+        vals.add("RSS ZDNet");
+        vals.add("RSS HackerNews");
+        vals.add("TODO list");
+        vals.add("Meteo");
+        vals.add("Calendar PRO");
+        vals.add("Calendar PERSO");
+        vals.add("Emails");
+        showList();
     }
 
     public void askCreds(View view) {
@@ -322,26 +371,26 @@ public class TaskLabAct extends FragmentActivity {
                         int itemPosition     = position;
                         // String  itemValue    = (String) listView.getItemAtPosition(position);
                         // si on veut editer en cliquant dessus:
-                        // editTask(itemPosition);
-                        showDetails(itemPosition);
+                        switch(list2action) {
+                            case 0: // menu principal: choix RSS, emails...
+                                choixmenu(itemPosition);
+                                break;
+                            case 1: // liste de RSS items: on veut le detail d'un item
+                                showDetails(itemPosition);
+                                break;
+                            case 2: // on edit une tache dans la TODO list
+                                editTask(itemPosition);
+                                break;
+                        }
                     }
                 });
             }
         });
     }
 
+    // shortcut button accessible on the first page
     public void fr3(View view) {
-        new AlertDialog.Builder(this)
-            .setTitle("Download France3 RSS")
-            .setMessage("Download FR3 RSS ?")
-            .setIcon(android.R.drawable.ic_dialog_alert)
-            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                    httpget("http://xolki.duckdns.org/rssf3?auth="+gitlabpwd);
-                }})
-        .setNegativeButton(android.R.string.no, null).show();
-    }
-    public void zdnet(View view) {
+        menuRSS("rssf3");
     }
     private int but2id = 0;
     public void reset(View view) {
@@ -350,18 +399,24 @@ public class TaskLabAct extends FragmentActivity {
                 askCreds(null);
                 break;
             case 1:
-                back2mainList();
+                pageRSSList();
                 break;
         }
     }
    
-    private void back2mainList() {
-        setButtons("FR3","SETUP","ZDnet");
+    private void pageRSSList() {
+        setButtons("Menu","SETUP","FR3");
         but2id=0;
         vals.clear();
         for (int i=0;i<curitems.getNitems();i++)
             vals.add(curitems.getTitle(i));
         showList();
+        main.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                listView.setSelectionFromTop(lastListPos,top);
+            }
+        });
     }
 
     private void httpget(final String url) {
